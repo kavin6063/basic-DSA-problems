533c70470439bf676958ab2448c4e95089183a46
Kavin <kavinkarthickeyan315@gmail.com>
2025-03-09 12:00:21 +0530

    2nd smallest

--- Basic DSA Problems/script.js
+++ Basic DSA Problems/script.js
@@ -26,23 +26,45 @@
 // return console.log(smallest);
 
 // 2ND LARGEST NO IN ARR
-const arr = [3, 5, 1, -7, 8, 6, 74, 9, 22];
+// const arr = [3, 5, 1, -7, 8, 6, 74, 9, 22];
 
-let largest = arr[0];
+// let largest = arr[0];
+// let length = arr.length;
+
+// for (let i = 1; i < length; i++) {
+//   if (arr[i] > largest) {
+//     largest = arr[i];
+//   }
+// }
+
+// let secondLargest = arr[0];
+
+// for (let i = 1; i < length; i++) {
+//   if (arr[i] < largest && arr[i] > secondLargest) {
+//     secondLargest = arr[i];
+//   }
+// }
+
+// return console.log(secondLargest);
+
+// 2ND SMALLEST NO IN ARR
+const arr = [3, 5, 1, 7, 8, 6, 74, 9, 22];
+
+let smallest = arr[0];
 let length = arr.length;
 
 for (let i = 1; i < length; i++) {
-  if (arr[i] > largest) {
-    largest = arr[i];
+  if (arr[i] < smallest) {
+    smallest = arr[i];
   }
 }
 
-let secondLargest = arr[0];
+let secondSmallest = arr[0];
 
 for (let i = 1; i < length; i++) {
-  if (arr[i] < largest && arr[i] > secondLargest) {
-    secondLargest = arr[i];
+  if (arr[i] > smallest && arr[i] < secondSmallest) {
+    secondSmallest = arr[i];
   }
 }
 
-return console.log(secondLargest);
+return console.log(secondSmallest);
