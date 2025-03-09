adfc12f2692d8e2d0efb6510c9fb1d651596b7d3
Kavin <kavinkarthickeyan315@gmail.com>
2025-03-09 11:48:19 +0530

    second largest

--- Basic DSA Problems/script.js
+++ Basic DSA Problems/script.js
@@ -26,13 +26,13 @@
 // return console.log(smallest);
 
 // 2ND LARGEST NO IN ARR
-const arr = [3, 5, 1, -7, 86, 6, 54, 9, 22];
+const arr = [3, 5, 1, -7, 8, 6, 74, 9, 22];
 
 let largest = arr[0];
 let length = arr.length;
 
 for (let i = 1; i < length; i++) {
-  if (largest < arr[i]) {
+  if (arr[i] > largest) {
     largest = arr[i];
   }
 }
