115c1fc4163fb3fba47a7cb5dc3d36fd83858a5a
Kavin <kavinkarthickeyan315@gmail.com>
2025-03-10 23:12:13 +0530

    reverse

--- Basic DSA Problems/script.js
+++ Basic DSA Problems/script.js
@@ -68,3 +68,15 @@
 // }
 
 // return console.log(secondSmallest);
+
+// REVERSE AN ARRAY
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = 0; i < arr.length / 2; i++) {
+//   let temp = arr[i];
+//   arr[i] = arr[arr.length - 1 - i];
+//   arr[arr.length - 1 - i] = temp;
+// }
+
+// return console.log(arr);
