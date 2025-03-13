b9131a90f331d1913381243d05c7daf914ea88e0
Kavin <kavinkarthickeyan315@gmail.com>
2025-03-13 23:35:19 +0530

    duplicates

--- Basic DSA Problems/script.js
+++ Basic DSA Problems/script.js
@@ -80,3 +80,19 @@
 // }
 
 // return console.log(arr);
+
+// duplicate in array
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = 0; i < arr.length; i++) {
+//   for (let j = i + 1; j < arr.length; j++) {
+//     if (arr[i] === arr[j]) {
+//       return console.log(arr[i]);
+//     }
+//   }
+// }
+
+// return console.log("no duplicates");
+
+//
