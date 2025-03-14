2525ac04e2184bd071dc6e111ac465158d71b447
Kavin <kavinkarthickeyan315@gmail.com>
2025-03-14 23:01:38 +0530

    sum

--- Basic DSA Problems/script.js
+++ Basic DSA Problems/script.js
@@ -94,3 +94,11 @@
 // }
 
 // return console.log("no duplicates");
+
+// SUM OF ARRAY
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   sum += arr[i];
+// }
+// return console.log(sum);
